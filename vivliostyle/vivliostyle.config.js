22dbf9d3a503e061fb265c1e87f4f4b98269c91d
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2026-02-12 23:07:51 +0900

    pronunciation key を書く

--- vivliostyle/vivliostyle.config.js
+++ vivliostyle/vivliostyle.config.js
@@ -21,6 +21,7 @@ const vivliostyleConfig = {
     '10_04_EN_authors.html',
     '10_08_EN_linzklar_start.html',
     '10_10_EN_how_to_use_linzklar.html',
+    '10_11_EN_key_to_pronunciation.html',
     '11_01_EN_目四片_清字.html',
     '11_02_EN_grammar_linzklar_abridged.html',
     '14_01_EN_pmcp_start.html',
